e66ef2d80369c303caec24470b017cd2ff957ef9
lnicosia <lucas.nicosia@yahoo.fr>
2020-02-15 17:41:07 +0100

    Power picker animation

---
+++ Assets/Scripts/power_picker.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class power_picker : MonoBehaviour
+{
+    private bool			isHeld = false;
+    private Vector3			offset;
+	public static double	fire_power = 5;
+	private Vector3			origin;
+	private Vector3			max;
+	private Vector3			min;
+	private Quaternion		origin_rotation;
+
+	void	Start()
+	{
+		this.origin = this.transform.localPosition;
+		this.origin_rotation = transform.localRotation;
+		this.min = this.origin + new Vector3(-GetComponent<SpriteRenderer>().size.x * 5, 0, 0);
+		this.max = this.origin + new Vector3(GetComponent<SpriteRenderer>().size.x * 5, GetComponent<SpriteRenderer>().size.y * 5, 0);
+		Debug.Log("Origin: " + this.origin);
+		Debug.Log("Min: " + this.min);
+		Debug.Log("Max: " + this.max);
+		
+	}
+
+    private Vector3 getWorldMouse()
+    {
+        Vector3 mouseWorldPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        return  new Vector3(
+                mouseWorldPos.x,
+                mouseWorldPos.y,
+                0);
+    }
+    // Start is called before the first frame update
+    private void OnMouseDown() {
+    	if (Input.GetMouseButtonDown(0)) {
+            Vector3 mouseWorldPos = getWorldMouse();
+    		isHeld = true;
+            offset = this.gameObject.transform.localPosition - mouseWorldPos;
+    	}
+    }
+    private void OnMouseUp() {
+    	if (Input.GetMouseButtonUp(0)) {
+    		isHeld = false;
+    	}
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isHeld) {
+        	Vector3 mouseWorldPos = getWorldMouse();
+
+        	transform.localPosition = new Vector3(
+        		Mathf.Clamp(mouseWorldPos.x + offset.x, min.x, max.x),
+        		transform.localPosition.y, 0);
+        	transform.localPosition = new Vector3(
+        		transform.localPosition.x,
+        		origin.y + Mathf.Abs(transform.localPosition.x - origin.x), 0);
+        	transform.localEulerAngles = new Vector3(0, 0, (transform.localPosition.x - origin.x) * 100);
+        	fire_power = transform.localPosition.x - origin.x;
+        	//Mathf.Clamp(this.gameObject.transform.localPosition.y, this.min.y, this.max.y),
+        	//	0);
+        	//Debug.Log("New pos = " + transform.localPosition);
+        }
+    }
+}
