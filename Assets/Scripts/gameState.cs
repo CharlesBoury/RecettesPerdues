ada704bc070aa6bc1110d7b1f2179a75be70535f
collion <charles.ollion@gmail.com>
2020-02-15 23:39:21 +0100

    adding game state and prefabs for everything

---
+++ Assets/Scripts/gameState.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gameState : MonoBehaviour
+{
+    public int state;
+    public List<GameObject> ingredients;
+    public GameObject ingredientsContainer;
+    public GameObject casserole;
+    // Start is called before the first frame update
+    void Start()
+    {
+      InitiateGame();
+    }
+
+    void InitiateGame()
+    {
+      state = 0;
+      foreach(GameObject objet in ingredients) {
+        GameObject instance = Instantiate(objet);
+        instance.transform.parent = ingredientsContainer.transform;
+        instance.GetComponent<Draggable>().casserole = casserole;
+      }
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
